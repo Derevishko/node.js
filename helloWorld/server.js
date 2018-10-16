7b90e45d9fcfab136f2e70693e615d5756255077
DerevishkoAndrey <derevishko@gmail.com>
2018-10-16 10:29:17 +0300

    hello world Koa.js

---
+++ helloWorld/server.js
@@ -0,0 +1,22 @@
+const koa = require('koa');
+const app = new koa();
+let map = new Map();
+
+map.set('/',async function(){
+  return `Hello Koa!!!`
+})
+
+
+map.set('/favicon.ico',async function(){
+  return `no image`
+})
+map.set('undefined', async function(){
+  return `error 404 not found`
+})
+
+app.use( async function(ctx, next) {
+  let url = ctx.request.url;
+  (map.has(url) ?  map.get(url)() : map.get('undefined')()).then((message)=>{ctx.body = message});
+});
+
+app.listen(3000)
